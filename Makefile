905b6ca33ead8757a4a6f57449a34b19ecd0c85f
Antoine Lassauzay <antoinelassauzay@MacBook-Air-de-Antoine-Lassauzay.local>
2012-12-05 00:06:35 -0500

    New exercices

--- Makefile
+++ Makefile
@@ -1,4 +1,10 @@
 CFLAGS=-Wall -g
 
+default: all
+
 clean:
-	rm -f ex1
+	rm -fr ex1 ex1.dSYM
+	rm -fr ex3 ex3.dSYM
+	rm -fr ex4 ex4.dSYM
+	
+all: ex1 ex3 ex4
---
+++ ex4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+	int age;
+	int height = 180;
+	
+	printf("I am %d years old\n", age);
+	printf("I am %d centimeters tall\n");
+}
